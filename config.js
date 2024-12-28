f182c82d9eeea3bcd02be2dc77a51c0c5749fc76
Divakar P S <divakarps@Divakars-MacBook-Pro.local>
2024-12-28 18:44:27 +0530

    added config file

---
+++ config.js
@@ -0,0 +1,5 @@
+export const clientId = "26e551bd-a1ad-4df6-85c2-01aac24507e1";
+export const clientSecret = "nix6b31wa4";
+export const authBaseUrl = "https://api.upstox.com/v2/login/authorization/dialog";
+export const tokenUrl = "https://api.upstox.com/v2/login/authorization/token";
+export const stockUrl = "https://api.upstox.com/v2/market-quote/ltp?instrument_key=NSE_EQ%7CINE848E01016";
